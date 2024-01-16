bcd5d26de5f815833bdbec371fe0391e6b08a533
Anish Bade <badeanish6045@gmail.com>
2024-01-17 00:25:29 +0545

    authentication and permissions

--- lms_api/main/urls.py
+++ lms_api/main/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
-urlpatterns=[
-    path('teacher/', views.TeacherList.as_view()),
-    path('teacher/<int:pk>/', views.TeacherDetail.as_view())
-    
+urlpatterns = [
+    path("teacher/", views.TeacherList.as_view()),
+    path("teacher/<int:pk>/", views.TeacherDetail.as_view()),
+    path("api-auth/", include("rest_framework.urls")),
 ]
--- lms_api/main/views.py
+++ lms_api/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import generics
+from rest_framework import generics, permissions   
 from main.serializers import TeacherSerializer
 from . import models
 
@@ -9,8 +9,9 @@ from . import models
 class TeacherList(generics.ListCreateAPIView):
     queryset = models.Teacher.objects.all()
     serializer_class = TeacherSerializer
-
+    permission_classes=[permissions.IsAuthenticated]
 
 class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Teacher.objects.all()
     serializer_class = TeacherSerializer
+    permission_classes=[permissions.IsAuthenticated]
