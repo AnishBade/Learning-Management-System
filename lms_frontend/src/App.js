adaad84780c2bcf4dbdf74f43bf0c3780c49eb34
Anish Bade <badeanish6045@gmail.com>
2024-01-17 22:56:33 +0545

    creating react templates with bootstrap

--- lms_frontend/src/App.js
+++ lms_frontend/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
-      Hello Anish !!!
+      <Main/>
     </div>
   );
 }
---
+++ lms_frontend/src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Home from './Home'
+import Header from './Header'
+export default function Main() {
+  return (
+    <div>
+      <Header/>
+      <Home/>
+    </div>
+  )
+}
