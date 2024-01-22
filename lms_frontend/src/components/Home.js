46693196e7af77a4a0b308abaddac62ea9bdd37e
Anish Bade <badeanish6045@gmail.com>
2024-01-22 22:34:16 +0545

    create homepage layout

--- lms_frontend/src/components/Home.js
+++ lms_frontend/src/components/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className='container mt-4'>
       {/* {Latest Courses} */}
-      <h3 className='pb-1 mb-4'>Latest Courses</h3>
+      <h3 className='pb-1 mb-4'>Latest Courses<a href='#' className='float-end'>See All</a></h3>
       <div className='row'>
         <div className='col-md-3'>
           <div className="card">
@@ -46,7 +46,7 @@ export default function Home() {
       {/* {End Latest Courses} */}
 
       {/* {Popular Courses} */}
-      <h3 className='pb-1 mb-4 mt-4'> Popular Courses</h3>
+      <h3 className='pb-1 mb-4 mt-4'> Popular Courses<a href='#' className='float-end'>See All</a></h3>
       <div className='row'>
         <div className='col-md-3'>
           <div className="card">
@@ -86,6 +86,102 @@ export default function Home() {
         </div>
       </div>     
       {/* {End Popular Courses} */}
+
+      {/* {Popular Teachers} */}
+      <h3 className='pb-1 mb-4 mt-4'> Popular Teachers<a href='#' className='float-end'>See All</a></h3>
+      <div className='row'>
+        <div className='col-md-3'>
+          <div className="card">
+            <a href='#' ><img src="logo512.png" className="card-img-top" alt="..."/></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href='#' >Course title</a></h5>
+            </div>
+          </div>
+
+        </div>
+        <div className='col-md-3'>
+          <div className="card">
+            <a href='#' ><img src="logo512.png" className="card-img-top" alt="..."/></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href='#' >Course title</a></h5>
+            </div>
+          </div>
+
+        </div>
+        <div className='col-md-3'>
+          <div className="card">
+            <a href='#' ><img src="logo512.png" className="card-img-top" alt="..."/></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href='#' >Course title</a></h5>
+            </div>
+          </div>
+
+        </div>
+        <div className='col-md-3'>
+          <div className="card">
+            <a href='#' ><img src="logo512.png" className="card-img-top" alt="..."/></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href='#' >Course title</a></h5>
+            </div>
+          </div>
+
+        </div>
+      </div>     
+      {/* {End Popular Teachers} */}
+
+      {/* {Student Testimonials} */}
+      <h3 className='pb-1 mb-4 mt-4'> Student Testimonials</h3>
+
+      <div id="carouselExampleIndicators" className="carousel slide bg-dark text-white py-5" data-bs-ride="true">
+        <div className="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <figure className="text-center">
+              <blockquote className="blockquote">
+                <p>A well-known quote, contained in a blockquote element.</p>
+              </blockquote>
+              <figcaption className="blockquote-footer">
+                Someone famous in <cite title="Source Title">First Carousel</cite>
+              </figcaption>
+            </figure>
+          </div>
+          <div className="carousel-item">
+            <figure className="text-center">
+              <blockquote className="blockquote">
+                <p>A well-known quote, contained in a blockquote element.</p>
+              </blockquote>
+              <figcaption className="blockquote-footer">
+                Someone famous in <cite title="Source Title">Second Carousel</cite>
+              </figcaption>
+            </figure>
+          </div>
+          <div className="carousel-item">
+            <figure className="text-center">
+              <blockquote className="blockquote">
+                <p>A well-known quote, contained in a blockquote element.</p>
+              </blockquote>
+              <figcaption className="blockquote-footer">
+                Someone famous in <cite title="Source Title">Third Carousel</cite>
+              </figcaption>
+            </figure>
+          </div>
+        
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+      
+      {/* {End Student Testimonials } */}
     </div>
     
   )
--- lms_frontend/src/components/Main.js
+++ lms_frontend/src/components/Main.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Home from './Home'
 import Header from './Header'
+import Footer from './Footer'
 export default function Main() {
   return (
     <div>
       <Header/>
       <Home/>
+      <Footer/>
     </div>
   )
 }
